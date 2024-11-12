323a9b475f183c6d8816039f7bf0dc79166fcb56
Corterwar <131016238+Corterwar@users.noreply.github.com>
2024-11-12 20:57:17 -0300

    Se agregaron Restricciones de la fecha

--- script/script_ddl_proyecto.sql
+++ script/script_ddl_proyecto.sql
@@ -20,7 +20,8 @@ CREATE TABLE Cliente
   Fecha_Registro DATE NOT NULL,
   Constraint PK_Cliente_IdCliente PRIMARY KEY (Id_Cliente),
   Constraint UQ_Cliente_Documento UNIQUE (Documento_Cliente),
-  Constraint UQ_Cliente_Correo UNIQUE (Correo)
+  Constraint UQ_Cliente_Correo UNIQUE (Correo),
+  Constraint DF_Cliente_Fecha Default getDate() for Fecha_Registro
 );
 
 CREATE TABLE Proveedor
@@ -34,7 +35,8 @@ CREATE TABLE Proveedor
   Fecha_Registro DATE NOT NULL,
   Constraint PK_Proveedor_IdProveedor PRIMARY KEY (Id_Proveedor),
   Constraint UQ_Proveedor_Documento UNIQUE (Documento_Proveedor),
-  Constraint UQ_Proveedor_Correo UNIQUE (Correo)
+  Constraint UQ_Proveedor_Correo UNIQUE (Correo),
+  Constraint DF_Proveedor_Fecha Default getDate() for Fecha_Registro
 );
 
 CREATE TABLE Rol
@@ -42,7 +44,8 @@ CREATE TABLE Rol
   Id_Rol INT IDENTITY NOT NULL,
   Descripcion VARCHAR(80) NOT NULL,
   Fecha_Registro DATE NOT NULL,
-  Constraint PK_Rol_IdRol PRIMARY KEY (Id_Rol)
+  Constraint PK_Rol_IdRol PRIMARY KEY (Id_Rol),
+  Constraint DF_Rol_Fecha Default getDate() for Fecha_Registro
 );
 
 CREATE TABLE Usuario
@@ -59,7 +62,8 @@ CREATE TABLE Usuario
   Constraint PK_Usuario_IdUsuario PRIMARY KEY (Id_Usuario),
   Constraint FK_Usuario_IdRol FOREIGN KEY (Id_Rol) REFERENCES Rol(Id_Rol),
   Constraint UQ_Usuario_Documento UNIQUE (Documento_Usuario),
-  Constraint UQ_Usuario_Correo UNIQUE (Correo)
+  Constraint UQ_Usuario_Correo UNIQUE (Correo),
+  Constraint DF_Usuario_Fecha Default getDate() for Fecha_Registro
 );
 
 CREATE TABLE Categorias
@@ -68,7 +72,8 @@ CREATE TABLE Categorias
   Descripcion_Categoria VARCHAR(100) NOT NULL,
   Estado INT NOT NULL,
   Fecha_Registro DATE NOT NULL,
-  Constraint PK_Categorias_IdCategoria PRIMARY KEY (Id_Categoría)
+  Constraint PK_Categorias_IdCategoria PRIMARY KEY (Id_Categoría),
+  Constraint DF_Categorias_Fecha Default getDate() for Fecha_Registro
 );
 
 CREATE TABLE Tipo_Documento
@@ -85,7 +90,8 @@ CREATE TABLE Permiso
   Fecha_Registro DATE NOT NULL,
   Id_Rol INT NOT NULL,
   Constraint PK_Permiso_IdPermiso PRIMARY KEY (Id_Permiso),
-  Constraint FK_Permiso_IdRol FOREIGN KEY (Id_Rol) REFERENCES Rol(Id_Rol)
+  Constraint FK_Permiso_IdRol FOREIGN KEY (Id_Rol) REFERENCES Rol(Id_Rol),
+  Constraint DF_Permiso_Fecha Default getDate() for Fecha_Registro
 );
 
 CREATE TABLE Productos
@@ -104,7 +110,8 @@ CREATE TABLE Productos
   Constraint PK_Productos_IdProducto PRIMARY KEY (Id_Producto),
   Constraint FK_Productos_IdCategoria FOREIGN KEY (Id_Categoría) REFERENCES Categorias(Id_Categoría),
   Constraint FK_Productos_IdProveedor FOREIGN KEY (Id_Proveedor) REFERENCES Proveedor(Id_Proveedor),
-  Constraint UQ_Productos_CodigoProd UNIQUE (Codigo_Producto)
+  Constraint UQ_Productos_CodigoProd UNIQUE (Codigo_Producto),
+  Constraint DF_Productos_Fecha Default getDate() for Fecha_Registro
 );
 
 CREATE TABLE Compra
@@ -118,7 +125,8 @@ CREATE TABLE Compra
   Constraint PK_Compra_IdCompra PRIMARY KEY (Id_Compra),
   Constraint FK_Compra_IdUsuario FOREIGN KEY (Id_Usuario) REFERENCES Usuario(Id_Usuario),
   Constraint FK_Compra_TipoDocumento FOREIGN KEY (Id_TipoDocumento) REFERENCES Tipo_Documento(Id_TipoDocumento),
-  Constraint UQ_Compra_NumeroDocumento UNIQUE (Numero_Documento)
+  Constraint UQ_Compra_NumeroDocumento UNIQUE (Numero_Documento),
+  Constraint DF_Compra_Fecha Default getDate() for Fecha_Registro
 );
 
 CREATE TABLE Venta
@@ -135,7 +143,8 @@ CREATE TABLE Venta
   Constraint FK_Venta_IdCliente FOREIGN KEY (Id_Cliente) REFERENCES Cliente(Id_Cliente),
   Constraint FK_Venta_IdUsuario FOREIGN KEY (Id_Usuario) REFERENCES Usuario(Id_Usuario),
   Constraint FK_Venta_TipoDocumento FOREIGN KEY (Id_TipoDocumento) REFERENCES Tipo_Documento(Id_TipoDocumento),
-  Constraint UQ_Venta_NumeroDocumento UNIQUE (Numero_Documento)
+  Constraint UQ_Venta_NumeroDocumento UNIQUE (Numero_Documento),
+  Constraint DF_Venta_Fecha Default getDate() for Fecha_Registro
 );
 
 CREATE TABLE Detalle_Compra
@@ -148,7 +157,8 @@ CREATE TABLE Detalle_Compra
   Id_Producto INT NOT NULL,
   Constraint PK_DetalleCompra_IdDetalle_IdCompra PRIMARY KEY (Id_DetalleCompra, Id_Compra),
   Constraint FK_DetalleCompra_IdCompra FOREIGN KEY (Id_Compra) REFERENCES Compra(Id_Compra),
-  Constraint FK_DetalleCompra_IdProducto FOREIGN KEY (Id_Producto) REFERENCES Productos(Id_Producto)
+  Constraint FK_DetalleCompra_IdProducto FOREIGN KEY (Id_Producto) REFERENCES Productos(Id_Producto),
+  Constraint DF_DetalleCompra_Fecha Default getDate() for Fecha_Registro
 );
 
 CREATE TABLE Detalle_Venta
@@ -161,5 +171,6 @@ CREATE TABLE Detalle_Venta
   Id_Producto INT NOT NULL,
   Constraint PK_DetalleVenta_IdDetalleVenta_IdVenta PRIMARY KEY (Id_DetalleVenta, Id_Venta),
   Constraint FK_Venta_IdVenta FOREIGN KEY (Id_Venta) REFERENCES Venta(Id_Venta),
-  Constraint FK_Venta_IdProducto FOREIGN KEY (Id_Producto) REFERENCES Productos(Id_Producto)
+  Constraint FK_Venta_IdProducto FOREIGN KEY (Id_Producto) REFERENCES Productos(Id_Producto),
+  Constraint DF_DetalleVenta_Fecha Default getDate() for Fecha_Registro
 );
